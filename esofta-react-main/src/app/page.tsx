3d273ce02a1aab1b67876d3a3d16ac1ddca85a46
RicardooFilho <ricardo.tagami17@gmail.com>
2024-06-14 22:02:35 -0300

    Finalizado

--- esofta-react-main/src/app/page.tsx
+++ esofta-react-main/src/app/page.tsx
@@ -32,6 +32,98 @@ export default function Home() {
                       Hydro Pump.
                   </p>
               </CardSection>
+              <CardSection titulo={"Curiosidades"}>
+                  <ul>
+                      <li>Squirtle é um dos Pokémon mais populares e adoráveis.</li>
+                      <li>
+                          Seu nome deriva das palavras "squirrel" (esquilo) e "turtle"
+                          (tartaruga).
+                      </li>
+                      <li>
+                          Squirtle é frequentemente escolhido por treinadores para começar sua
+                          jornada Pokémon.
+                      </li>
+                  </ul>
+              </CardSection>
+              <CardSection titulo={"Squirtle: O Amigo Aquático"}>
+                  <p>
+                      Squirtle, com sua aparência simpática e sua habilidade em controlar a
+                      água, conquistou o coração de treinadores Pokémon ao redor do mundo.
+                      Sendo o inicial de água na região de Kanto, Squirtle é uma escolha
+                      popular para aqueles que buscam equilíbrio e versatilidade em suas
+                      equipes.
+                  </p>
+                  <p>
+                      Sua concha nas costas não apenas oferece proteção, mas também é um
+                      símbolo de resistência. Ao evoluir para Wartortle e, posteriormente,
+                      para Blastoise, Squirtle se transforma em uma força formidável,
+                      dominando ataques aquáticos que podem surpreender adversários em
+                      batalhas.
+                  </p>
+                  <p>
+                      Além de suas habilidades de batalha, Squirtle é conhecido por seu
+                      carisma. Treinadores muitas vezes descrevem a relação com seu Squirtle
+                      como uma amizade profunda, tornando-o não apenas um companheiro de
+                      lutas, mas um amigo leal ao longo de suas jornadas.
+                  </p>
+              </CardSection>
+              <CardSection titulo={"Recursos Adicionais"}>
+                  <ul>
+                      <li>
+                          <a
+                              href="https://www.pokemon.com/br/pokedex/squirtle"
+                              target="_blank"
+                          >Pokédex - Squirtle</a
+                          >
+                      </li>
+                      <li>
+                          <a
+                              href="https://bulbapedia.bulbagarden.net/wiki/Squirtle_(Pok%C3%A9mon)"
+                              target="_blank"
+                          >Bulbapedia - Squirtle</a
+                          >
+                      </li>
+                  </ul>
+              </CardSection>
+              <CardSection titulo={"Evoluções"}>
+                  <ul>
+                      <li>
+                          <a href="./pages/pokemon/index.html?name=squirtle">
+                              <figure>
+                                  <img
+                                      src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png"
+                                      alt="Squirtle"
+                                  />
+                                  <figcaption>1. Squirtle</figcaption>
+                              </figure>
+                          </a>
+                      </li>
+
+                      <li>
+                          <a href="./pages/pokemon/index.html?name=wartortle">
+                              <figure>
+                                  <img
+                                      src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png"
+                                      alt="Wartortle"
+                                  />
+                                  <figcaption>2. Wartortle</figcaption>
+                              </figure>
+                          </a>
+                      </li>
+
+                      <li>
+                          <a href="./pages/pokemon/index.html?name=blastoise">
+                              <figure>
+                                  <img
+                                      src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png"
+                                      alt="Blastoise"
+                                  />
+                                  <figcaption>3. Blastoise</figcaption>
+                              </figure>
+                          </a>
+                      </li>
+                  </ul>
+              </CardSection>
           </main>
       </>
 
